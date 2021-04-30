5b40841ab09bc8cf22db3d2f3245080f5fafad3d
gohanxl <Lacquaniti1961>
2021-04-30 01:01:28 -0300

    feat(App.store.js): add middlewares

--- src/App.store.js
+++ src/App.store.js
@@ -1,8 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+
 import { homeReducer } from "./modules/home/store/home.reducer";
 
-export default configureStore({
-  reducer: {
-    home: homeReducer,
-  },
-});
+export function configureAppStore(preloadedState) {
+  const store = configureStore({
+    reducer: {
+      home: homeReducer,
+    },
+    middleware: [thunk, logger],
+    preloadedState,
+  });
+
+  return store;
+}
