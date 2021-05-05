8d96e2b68e8c0419cee8bf57716a1d0d4590aaf3
Lucas Lacquaniti [gohanxl] <lucasmlacquaniti@gmail.com>
2021-05-04 22:43:23 -0300

    feat(Home.component.js): add mock html to test Bulma

--- src/modules/home/components/Home.component.js
+++ src/modules/home/components/Home.component.js
@@ -1,11 +1,43 @@
 import React from "react";
+import "./Home.styles.scss"
 
 export const Home = ({ count, addToCounter }) => {
   return (
-    <div>
-      <div>{count}</div>
-      <div onClick={() => addToCounter()}>Increase</div>
-      <div>Decrease</div>
-    </div>
+    <>
+      <div>
+        <div>{count}</div>
+        <div onClick={() => addToCounter()}>Increase</div>
+        <div>Decrease</div>
+      </div>
+      <h1 class="title">Bulma</h1>
+
+      <p class="subtitle">
+        Modern CSS framework based on{" "}
+        <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Flexible_Box_Layout/Basic_Concepts_of_Flexbox">
+          Flexbox
+        </a>
+      </p>
+
+      <div class="field">
+        <div class="control">
+          <input class="input" type="text" placeholder="Input" />
+        </div>
+      </div>
+
+      <div class="field">
+        <p class="control">
+          <span class="select">
+            <select>
+              <option>Select dropdown</option>
+            </select>
+          </span>
+        </p>
+      </div>
+
+      <div class="buttons">
+        <a class="button is-primary">Primary</a>
+        <a class="button is-link">Link</a>
+      </div>
+    </>
   );
 };
