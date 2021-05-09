956ab7a0370d544cc433b7018c055e761d9df122
Marcelo Serrano <marcelo@dive.games>
2021-05-09 17:43:02 -0300

    comment api call

--- src/views/student/StudentLecture/StudentLecture.view.renderer.js
+++ src/views/student/StudentLecture/StudentLecture.view.renderer.js
@@ -3,21 +3,23 @@ import {useAuth0} from "@auth0/auth0-react";
 
 export const StudentLectureViewRenderer = () => {
 
-    const {
-        user,
-        isAuthenticated,
-       getAccessTokenSilently
-    } = useAuth0();
+    //TODO Remove this later, this comment explains how to call backend api
 
-    getAccessTokenSilently({
-        audience: '5coding-backend',
-        scope: "ADMIN",
-    }).then(res => fetch('http://localhost:5000/api/Institutos', {
-        headers: {
-            Authorization: `Bearer ${res}`
-        }}))
-        .then(res => console.log(res.json()))
-        .catch(err => console.log(err));
+    // const {
+    //     user,
+    //     isAuthenticated,
+    //    getAccessTokenSilently
+    // } = useAuth0();
+    //
+    // getAccessTokenSilently({
+    //     audience: '5coding-backend',
+    //     scope: "ADMIN",
+    // }).then(res => fetch('http://localhost:5000/api/Institutos', {
+    //     headers: {
+    //         Authorization: `Bearer ${res}`
+    //     }}))
+    //     .then(res => console.log(res.json()))
+    //     .catch(err => console.log(err));
 
   return (
     <div>
