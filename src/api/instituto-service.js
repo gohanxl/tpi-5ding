e3333a4c30c9c085b800c3d071f87cfdd2c98297
Lucas Lacquaniti [gohanxl] <lucasmlacquaniti@gmail.com>
2021-05-12 23:10:00 -0300

    feat(instituto-service.js): remove test

--- src/api/instituto-service.js
+++ src/api/instituto-service.js
@@ -3,5 +3,3 @@ import axios from 'axios';
 export const institutosService = {
   getInstitutos: (auth_token) => axios.get(`${window._env_.API_URL}/Institutos`, {Headers: {'Authorization': auth_token}}),
 }
-
-console.log('husky test')
