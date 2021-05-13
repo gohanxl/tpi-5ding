b7537f04bf9bd257be808100f4c6a9fc4797ceb3
Lucas Lacquaniti [gohanxl] <lucasmlacquaniti@gmail.com>
2021-05-12 23:42:10 -0300

    Husky test 2

--- src/api/instituto-service.js
+++ src/api/instituto-service.js
@@ -1,5 +1,10 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const institutosService = {
-  getInstitutos: (auth_token) => axios.get(`${window._env_.API_URL}/Institutos`, {Headers: {'Authorization': auth_token}}),
-}
+  getInstitutos: (auth_token) =>
+    axios.get(`${window._env_.API_URL}/Institutos`, {
+      Headers: { Authorization: auth_token },
+    }),
+};
+
+console.log("huskyTEtestest");
