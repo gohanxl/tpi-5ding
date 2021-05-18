00b062a6d7d31cc74c67a7330cb32acaa94f2fc9
Lucas Lacquaniti [gohanxl] <lucasmlacquaniti@gmail.com>
2021-05-18 00:05:35 -0300

    feat(ChatWindows.component.js): add delete message

--- src/modules/shared-components/Chat/components/ChatWindow/ChatWindows.component.js
+++ src/modules/shared-components/Chat/components/ChatWindow/ChatWindows.component.js
@@ -2,16 +2,18 @@ import React from "react";
 import { chat_wrapper } from "./ChatWindows.module.scss";
 import { Message } from "../ChatMessage/ChatMessage.component";
 
-export const ChatWindow = ({ chat }) => {
+export const ChatWindow = ({ chat, deleteMessage }) => {
   return (
     <div className={chat_wrapper}>
       {chat &&
         chat.map(({ id, user, message, messageDate }) => (
           <Message
             key={id}
+            userId={id}
             user={user}
             messageDate={messageDate}
             message={message}
+            deleteMessage={deleteMessage}
           />
         ))}
     </div>
