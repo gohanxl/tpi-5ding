a4ed9df817a832f0f0b4e000694d9fbc163d8ebd
Lucas Lacquaniti [gohanxl] <lucasmlacquaniti@gmail.com>
2021-05-18 00:04:54 -0300

    feat(ChatMessage.component.js): add new props to message

--- src/modules/shared-components/Chat/components/ChatMessage/ChatMessage.component.js
+++ src/modules/shared-components/Chat/components/ChatMessage/ChatMessage.component.js
@@ -1,11 +1,18 @@
 import React from "react";
 
-export const Message = ({ user, message, messageDate }) => (
+export const Message = ({
+  userId,
+  user,
+  message,
+  messageDate,
+  deleteMessage,
+}) => (
   <div style={{ background: "#eee", borderRadius: "5px", padding: "0 10px" }}>
     <p>
       <span>{messageDate} --- </span>
       <strong>{user}</strong> says:
     </p>
     <p>{message}</p>
+    <button onClick={() => deleteMessage(userId, user)}>Borrar Mensaje</button>
   </div>
 );
