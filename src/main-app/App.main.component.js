96c72d27167ba7a4ea29d8159ff51b17519dac4a
Lucas Lacquaniti [gohanxl] <lucasmlacquaniti@gmail.com>
2021-05-22 22:47:22 -0300

    feat(App.main.component.js): change html

--- src/main-app/App.main.component.js
+++ src/main-app/App.main.component.js
@@ -3,12 +3,11 @@ import { Sidebar } from "./modules/shared-components/Sidebar/Sidebar.component";
 import { MainAppRoutes } from "./App.main.routes";
 
 const MainApp = () => {
-  //TODO remove href must work as SPA
   return (
-    <div>
+    <>
       <Sidebar />
       <MainAppRoutes />
-    </div>
+    </>
   );
 };
 
