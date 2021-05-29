8a1d4d70a64b23834c83b0a920c5d9477e2c9ef9
Marcelo Serrano <marcelo@dive.games>
2021-05-29 13:35:46 -0300

    teacher subjects component

--- src/main-app/views/teacher/TeacherAssignature/TeacherAssignature.view.renderer.js
+++ src/main-app/views/teacher/TeacherAssignature/TeacherAssignature.view.renderer.js
@@ -1,9 +1,52 @@
 import React from "react";
+import { CourseSubjectComponent } from "../../../modules/teacher/components/CourseSubject.component";
+import historySvg from "../../../../assets/img/education/007-history.svg";
+import geographySvg from "../../../../assets/img/education/008-geography.svg";
 
 export const TeacherAssignatureViewRenderer = () => {
+  const thisDataMustComeFromServer = [
+    {
+      course: "5to A",
+      subjects: [
+        {
+          name: "GEOGRAFÍA",
+          image: historySvg,
+          route: "/educapp/student",
+        },
+        {
+          name: "HISTORIA",
+          image: geographySvg,
+          route: "/educapp/student",
+        },
+      ],
+    },
+    {
+      course: "6to B",
+      subjects: [
+        {
+          name: "GEOGRAFÍA",
+          image: historySvg,
+          route: "/educapp/student",
+        },
+        {
+          name: "HISTORIA",
+          image: geographySvg,
+          route: "/educapp/student",
+        },
+      ],
+    },
+  ];
+
   return (
-    <div>
-      <h1>MESSIRVE EL PEBETE TEACHER ASSIGNATURATION</h1>
-    </div>
+    <section>
+      {thisDataMustComeFromServer.map((data) => (
+        <CourseSubjectComponent
+          key={data.course}
+          course={data.course}
+          subjects={data.subjects}
+        />
+      ))}
+      <br />
+    </section>
   );
 };
