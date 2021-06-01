4e4a14e1305527a7230f34fac83e09eb638c15e9
micavsr <anamicaela2008@gmail.com>
2021-06-01 01:18:55 -0300

    Fix camera view, use user name for the camera

--- src/main-app/views/Call/CallViewRenderer.js
+++ src/main-app/views/Call/CallViewRenderer.js
@@ -1,19 +1,21 @@
 /* eslint-disable */
 import React from "react";
-import { VideoChatComponent } from "../../modules/videocall/video-chat/VideoChatComponent";
-import { ClosedCaptionComponent } from "../../modules/videocall/closed-caption/ClosedCaption.component";
+import { VideoChat } from "../../modules/videocall/video-chat/VideoChat.component";
 import { v4 } from "uuid";
+import { ClosedCaptionComponent } from "../../modules/videocall/closed-caption/ClosedCaption.component";
 
-export const CallViewRenderer = () => {
+export const CallViewRenderer = ({ currentUser }) => {
   const uuid = v4();
+  const userName =
+    currentUser && currentUser.dbUser ? currentUser.dbUser.Nombre : "";
 
   return (
     <section>
       <div>
-        <VideoChatComponent name={uuid} meeting="1" />
+        <VideoChat name={userName} uuid={uuid} meeting="1" />
       </div>
       <div>
-        <ClosedCaptionComponent name={uuid} meeting="1" />
+        <ClosedCaptionComponent name={userName} meeting="1" />
       </div>
     </section>
   );
