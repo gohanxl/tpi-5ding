32e969de79fe12063bb0df3ddb98db7c02f5d18c
Lucas Lacquaniti [gohanxl] <lucasmlacquaniti@gmail.com>
2021-06-01 21:13:59 -0300

    feat(App.main.routes.js): change dashboard routing

--- src/main-app/App.main.routes.js
+++ src/main-app/App.main.routes.js
@@ -1,7 +1,7 @@
 import React, { lazy } from "react";
 import { Route, Switch, Redirect } from "react-router";
 import { routes } from "../App.constants";
-import { NotFound } from "./modules/shared-components/NotFound/NotFound.component";
+// import { NotFound } from "./modules/shared-components/NotFound/NotFound.component";
 
 const HomeContainer = lazy(() =>
   import("./modules/home/components/Home.container")
@@ -10,12 +10,15 @@ const HomeContainer = lazy(() =>
 export const MainAppRoutes = ({ routesRoleConfig }) => {
   return (
     <Switch>
-      <Redirect exact from="/" to="/home" />
       {routesRoleConfig.map((routeProps) => (
         <Route key={routeProps.path} {...routeProps} />
       ))}
-      <Route key="home" path={routes.dashboard} component={HomeContainer} />
-      <Route component={NotFound} />
+      <Route
+        key="dashboard"
+        path={routes.dashboard}
+        component={HomeContainer}
+      />
+      <Redirect to={routes.dashboard} component={HomeContainer} />
     </Switch>
   );
 };
