486e5aaf7bafea7b4d8ba84396454514a279e2b3
Lucas Lacquaniti [gohanxl] <lucasmlacquaniti@gmail.com>
2021-06-18 00:05:13 -0300

    feat(App.constants.js): add app constants

---
+++ src/App.constants.js
@@ -0,0 +1,29 @@
+export const rootStyles = [
+  "--primary",
+  "--secondary",
+  "--darker-primary",
+  "--dark-primary",
+  "--light-aquamarine",
+  "--aquamarine",
+  "--light-blue",
+];
+
+export const standardThemeValues = [
+  "#1eaac3",
+  "#f5dc17",
+  "#1f4d57",
+  "#24778a",
+  "#aadbc7",
+  "#8bd2b0",
+  "#b8e3ec",
+];
+
+export const colorblindModeThemeValues = {
+  "--primary": "black",
+  "--secondary": "black",
+  "--darker-primary": "black",
+  "--dark-primary": "black",
+  "--light-aquamarine": "black",
+  "--aquamarine": "black",
+  "--light-blue": "black",
+};
