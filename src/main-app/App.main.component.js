5fb26402a32126f7e50a276c453a9fb0dcd817e9
Marcelo Serrano <marcelo@dive.games>
2021-06-21 15:00:56 -0300

    fix class for className it was really annoying

--- src/main-app/App.main.component.js
+++ src/main-app/App.main.component.js
@@ -69,96 +69,98 @@ const MainApp = () => {
   return (
     <div className={isLoading ? spinning_svg : ""}>
       {isLoading ? (
-          <Loader />
+        <Loader />
       ) : (
-      <>
-        <header>
-          <nav
-            className="educapp-nav navbar"
-            role="navigation"
-            aria-label="main navigation"
-          >
-            <div className="navbar-brand">
-              <a className="navbar-item" href="#/educapp/home">
-                <img
-                  src={educAppWhiteLogo}
-                  alt="Educapp logo"
-                  width="30"
-                  height="30"
-                />
-              </a>
-            </div>
+        <>
+          <header>
+            <nav
+              className="educapp-nav navbar"
+              role="navigation"
+              aria-label="main navigation"
+            >
+              <div className="navbar-brand">
+                <a className="navbar-item" href="#/educapp/home">
+                  <img
+                    src={educAppWhiteLogo}
+                    alt="Educapp logo"
+                    width="30"
+                    height="30"
+                  />
+                </a>
+              </div>
 
-            {isAuthenticated ? (
-              <div class="navbar-end">
-                <div class="navbar-item has-dropdown is-hoverable">
-                  <a class="navbar-link is-arrowless">
-                    <FontAwesomeIcon color="white" size="lg" icon={faBell} />
-                  </a>
-                  <div class="navbar-dropdown is-right">
-                    <a class="navbar-item">Ir a clases</a>
-                    <a class="navbar-item">Examen de lengua</a>
-                    <a class="navbar-item">Entregar tarea de Historia</a>
+              {isAuthenticated ? (
+                <div className="navbar-end">
+                  <div className="navbar-item has-dropdown is-hoverable">
+                    <a className="navbar-link is-arrowless">
+                      <FontAwesomeIcon color="white" size="lg" icon={faBell} />
+                    </a>
+                    <div className="navbar-dropdown is-right">
+                      <a className="navbar-item">Ir a clases</a>
+                      <a className="navbar-item">Examen de lengua</a>
+                      <a className="navbar-item">Entregar tarea de Historia</a>
+                    </div>
+                  </div>
+                  <div className="navbar-item has-dropdown is-hoverable">
+                    <a className="navbar-link is-arrowless">
+                      <FontAwesomeIcon color="white" size="lg" icon={faUser} />
+                    </a>
+                    <div className="navbar-dropdown is-right">
+                      <a className="navbar-item">Mi perfil</a>
+                      <hr className="dropdown-divider"></hr>
+                      <a
+                        className="navbar-item"
+                        onClick={() =>
+                          logout({ returnTo: window.location.origin })
+                        }
+                      >
+                        Cerrar sesión
+                      </a>
+                    </div>
                   </div>
                 </div>
-                <div class="navbar-item has-dropdown is-hoverable">
-                  <a class="navbar-link is-arrowless">
-                    <FontAwesomeIcon color="white" size="lg" icon={faUser} />
-                  </a>
-                  <div class="navbar-dropdown is-right">
-                    <a class="navbar-item">Mi perfil</a>
-                    <hr class="dropdown-divider"></hr>
-                    <a
-                      class="navbar-item"
+              ) : (
+                <div className="navbar-end">
+                  <div className="navbar-item">
+                    <button
+                      className="btn btn-warning"
                       onClick={() =>
                         logout({ returnTo: window.location.origin })
                       }
                     >
-                      Cerrar sesión
-                    </a>
+                      Log Out
+                    </button>
                   </div>
                 </div>
-              </div>
-            ) : (
-              <div class="navbar-end">
-                <div class="navbar-item">
-                  <button
-                    className="btn btn-warning"
-                    onClick={() => logout({ returnTo: window.location.origin })}
-                  >
-                    Log Out
-                  </button>
-                </div>
-              </div>
-            )}
-          </nav>
-        </header>
-        <div className="App is-flex">
-          <div>
-            <Sidebar />
-          </div>
-          <div
-            className={"app-container" + (!hideFooter ? "" : " hide-footer")}
-          >
-            <div className="app-content">
-              {isAuthenticated && <MainAppRoutes />}
-              {!isAuthenticated && loginWithRedirect()}
+              )}
+            </nav>
+          </header>
+          <div className="App is-flex">
+            <div>
+              <Sidebar />
             </div>
-            <footer className="footer">
-              <p className="has-text-centered">
-                <span>Powered by</span>
-                <img
-                  src={educAppLogo}
-                  className="mx-2 mb-1"
-                  alt="Educapp logo"
-                  width="20"
-                />
-                <b>EducApp</b>
-              </p>
-            </footer>
+            <div
+              className={"app-container" + (!hideFooter ? "" : " hide-footer")}
+            >
+              <div className="app-content">
+                {isAuthenticated && <MainAppRoutes />}
+                {!isAuthenticated && loginWithRedirect()}
+              </div>
+              <footer className="footer">
+                <p className="has-text-centered">
+                  <span>Powered by</span>
+                  <img
+                    src={educAppLogo}
+                    className="mx-2 mb-1"
+                    alt="Educapp logo"
+                    width="20"
+                  />
+                  <b>EducApp</b>
+                </p>
+              </footer>
+            </div>
           </div>
-        </div>
-      </>
+        </>
       )}
     </div>
   );
