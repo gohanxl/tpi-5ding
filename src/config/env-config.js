6f466dca37469d1afc370614a1ab0da86a7d50f9
Marcelo Serrano <marcelo@dive.games>
2021-06-23 22:34:27 -0300

    .env env-config and peerjs for prod

--- src/config/env-config.js
+++ src/config/env-config.js
@@ -8,8 +8,8 @@ export const env_config = {
   domain: REACT_APP_AUTH0_DOMAIN,
   clientId: REACT_APP_AUTH0_CLIENT_ID,
   // redirectUri: window.location.origin,
-  // redirectUri: "https://educapp.net.ar/#/educapp",
-  redirectUri: "http://localhost:3000/#/educapp",
+  redirectUri: "https://educapp.net.ar/#/educapp",
+  // redirectUri: "http://localhost:3000/#/educapp",
   audience: REACT_APP_AUTH0_AUDIENCE,
 };
 
