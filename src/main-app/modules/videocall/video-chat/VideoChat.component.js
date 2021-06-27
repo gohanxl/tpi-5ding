80bb8b7e788c2affab7d7d2cbc2d90e21e28776b
Marcelo Serrano <marcelo@dive.games>
2021-06-27 12:47:53 -0300

    env + config for prod

--- src/main-app/modules/videocall/video-chat/VideoChat.component.js
+++ src/main-app/modules/videocall/video-chat/VideoChat.component.js
@@ -582,7 +582,35 @@ export const VideoChat = (props) => {
       path: process.env.REACT_APP_PEERJS_PATH,
       host: process.env.REACT_APP_PEERJS_HOST,
       port: process.env.REACT_APP_PEERJS_PORT,
-      debug: 0,
+      config: {
+        iceServers: [
+          {
+            url: "turn:turn.anyfirewall.com:443?transport=tcp",
+            credential: "webrtc",
+            username: "webrtc",
+          },
+          {
+            url: "turn:numb.viagenie.ca",
+            credential: "muazkh",
+            username: "webrtc@live.com",
+          },
+          {
+            url: "turn:192.158.29.39:3478?transport=udp",
+            credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+            username: "28224511:1379330808",
+          },
+          {
+            url: "turn:192.158.29.39:3478?transport=tcp",
+            credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+            username: "28224511:1379330808",
+          },
+          {
+            url: "turn:turn.bistri.com:80",
+            credential: "homeo",
+            username: "homeo",
+          },
+        ],
+      },
     });
   };
 
