2ddbb8a5aa69bc83b3a2ceabc0c6bd154d010984
Marcelo Serrano <marcelo@dive.games>
2021-07-04 15:28:58 -0300

    delete activity

--- src/main-app/modules/teacher/components/course/activity/api/activity-service.js
+++ src/main-app/modules/teacher/components/course/activity/api/activity-service.js
@@ -11,4 +11,8 @@ export const activityService = {
     axios.get(`${process.env.REACT_APP_API_URL}/Actividad/clase/${classId}`, {
       headers: { Authorization: "Bearer " + auth_token },
     }),
+  deleteActivityById: (auth_token, activityId) =>
+    axios.delete(`${process.env.REACT_APP_API_URL}/Actividad/${activityId}`, {
+      headers: { Authorization: "Bearer " + auth_token },
+    }),
 };
--- src/main-app/modules/teacher/components/course/activity/components/Activity.component.js
+++ src/main-app/modules/teacher/components/course/activity/components/Activity.component.js
@@ -16,41 +16,9 @@ export const ActivityComponent = () => {
 
   const user = useSelector((state) => state.user.currentUser);
   // eslint-disable-next-line
+
   const [data, setData] = useState([]);
-
-  //TODO onClick actions
-  const actions = (actividad) => {
-    return (
-      <div key={1} className="buttons are-small">
-        {actividad.FilePath && (
-          <button className="button is-info is-light">
-            <FontAwesomeIcon icon={faDownload} />
-          </button>
-        )}
-        {!actividad.FilePath && (
-          <button className="button is-info is-light" disabled>
-            <FontAwesomeIcon icon={faDownload} />
-          </button>
-        )}
-
-        <button className="button is-success is-light">
-          <FontAwesomeIcon icon={faPencilAlt} />
-        </button>
-        <button className="button is-danger is-light">
-          <FontAwesomeIcon icon={faTrash} />
-        </button>
-      </div>
-    );
-  };
-
-  const formatDate = (dateToFormat) => {
-    const splitDate = dateToFormat.split("T");
-    const date = splitDate[0];
-    const hours = splitDate[1].split(".")[0];
-    return date + " " + hours;
-  };
-
-  useEffect(() => {
+  const getActivities = () => {
     activityService
       .getActivityByClassId(user.token, 1)
       .then((res) => {
@@ -74,6 +42,50 @@ export const ActivityComponent = () => {
         setData(data);
       })
       .catch((err) => console.error(err));
+  };
+
+  const deleteActivity = (id) => {
+    activityService
+      .deleteActivityById(user.token, id)
+      .then((res) => {
+        console.log(res.data);
+        getActivities();
+      })
+      .catch((err) => console.error(err));
+  };
+
+  //TODO onClick actions
+  const actions = (actividad) => {
+    return (
+      <div key={1} className="buttons are-small">
+        <button
+          className="button is-info is-light"
+          disabled={!actividad.FilePath}
+        >
+          <FontAwesomeIcon icon={faDownload} />
+        </button>
+        <button className="button is-success is-light">
+          <FontAwesomeIcon icon={faPencilAlt} />
+        </button>
+        <button
+          className="button is-danger is-light"
+          onClick={() => deleteActivity(actividad.Id)}
+        >
+          <FontAwesomeIcon icon={faTrash} />
+        </button>
+      </div>
+    );
+  };
+
+  const formatDate = (dateToFormat) => {
+    const splitDate = dateToFormat.split("T");
+    const date = splitDate[0];
+    const hours = splitDate[1].split(".")[0];
+    return date + " " + hours;
+  };
+
+  useEffect(() => {
+    getActivities();
   }, [user]);
 
   const history = useHistory();
