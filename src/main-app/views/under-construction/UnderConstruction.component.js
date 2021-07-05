026bb1c304dbcff8704026edcbcc110190ffc364
Lucas Lacquaniti [gohanxl] <lucasmlacquaniti@gmail.com>
2021-07-04 21:32:01 -0300

    feat(UnderConstruction): create component

---
+++ src/main-app/views/under-construction/UnderConstruction.component.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const UnderConstruction = () => {
+  return (
+    <div>
+      <div>SVG</div>
+      <p>¡Estamos en pleno desarrollo, muchas gracias por el apoyo!</p>
+    </div>
+  );
+};
