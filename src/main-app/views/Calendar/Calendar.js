3e1b38e12a2a2ce62e2e882f7843837278f4474f
Isaias Caporusso <isaias.caporusso@gmail.com>
2021-07-05 00:45:25 -0300

    fix time and add delete event

--- src/main-app/views/Calendar/Calendar.js
+++ src/main-app/views/Calendar/Calendar.js
@@ -1,5 +1,5 @@
 /*eslint-disable*/
-import React from "react";
+import React, { useEffect } from "react";
 import { Calendar as BigCalendar, dateFnsLocalizer } from "react-big-calendar";
 import format from "date-fns/format";
 import parse from "date-fns/parse";
@@ -7,8 +7,10 @@ import startOfWeek from "date-fns/startOfWeek";
 import getDay from "date-fns/getDay";
 import { es } from "date-fns/locale";
 import SweetAlert from "react-bootstrap-sweetalert";
+import { calendarService } from "./Calendar.service";
 
 import "./Calendar.styles.scss";
+import { useSelector } from "react-redux";
 
 const calendarEvents = [];
 
@@ -39,44 +41,137 @@ const localizer = dateFnsLocalizer({
 });
 
 export const Calendar = (props) => {
-  const [events, setEvents] = React.useState(calendarEvents);
+  const user = useSelector((state) => state.user.currentUser);
+
+  const [events, setEvents] = React.useState([]);
   const [alert, setAlert] = React.useState(null);
+  let title = "";
+  let description = "";
+
+  function convertUTCDateToLocalDate(date) {
+    var newDate = new Date(
+      date.getTime() - date.getTimezoneOffset() * 60 * 1000
+    );
+    return newDate;
+  }
+
+  const getAndSetEvents = () => {
+    calendarService
+      .getEvents(user.token, user.dbUser.Id)
+      .then((response) => {
+        response.data.events.map((event) => {
+          event.start = convertUTCDateToLocalDate(new Date(event.start));
+          event.end = convertUTCDateToLocalDate(new Date(event.end));
+        });
+        setEvents(response.data.events);
+      })
+      .catch((e) => console.error(e));
+  };
+
+  useEffect(() => {
+    if (user && user.dbUser) {
+      getAndSetEvents();
+    }
+  }, [user]);
+
   const selectedEvent = (event) => {
     setAlert(
       <SweetAlert
+        showCancel
+        showCloseButton
+        confirmBtnCssClass="btn-confirm"
+        cancelBtnText="Eliminar"
+        confirmBtnText="Cerrar"
+        cancelBtnBsStyle="danger"
         title={event.title}
         onConfirm={() => hideAlert()}
-        onCancel={() => hideAlert()}
+        onCancel={() => deleteEvent(event)}
       >
+        {event.description}
+        <hr />
         {event.start.toLocaleTimeString().slice(0, -3)} -{" "}
         {event.end.toLocaleTimeString().slice(0, -3)}
       </SweetAlert>
     );
   };
-  const addNewEventAlert = (slotInfo) => {
+
+  const deleteEvent = (event) => {
+    calendarService.deleteEvent(user.token, event.id).then(
+      setTimeout(() => {
+        getAndSetEvents();
+      }, 2000)
+    );
+    successAlert("El evento se elimino correctamente");
+  };
+
+  const successAlert = (message) => {
     setAlert(
       <SweetAlert
-        input
-        showCancel
-        cancelBtnBsStyle="danger"
+        success
         confirmBtnCssClass="btn-confirm"
-        title="Agrega tu evento"
-        cancelBtnText="Cancelar"
-        confirmBtnText="Guardar"
-        onConfirm={(e) => addNewEvent(e, slotInfo)}
-        onCancel={() => hideAlert()}
+        confirmBtnText="Cerrar"
+        title={message}
+        onConfirm={() => hideAlert()}
       />
     );
   };
+
+  const addNewEventAlert = (slotInfo) => {
+    setAlert(
+      <SweetAlert
+        title="Agregar evento"
+        showCancel
+        onConfirm={(e) => addNewEvent(e, slotInfo)}
+        onCancel={() => hideAlert()}
+        cancelBtnBsStyle="danger"
+        confirmBtnCssClass="btn-confirm"
+        cancelBtnText="Cancelar"
+        confirmBtnText="Guardar"
+        type={"controlled"}
+        dependencies={[title, description]}
+      >
+        {(renderProps) => (
+          <form>
+            <input
+              type={"text"}
+              ref={renderProps.setAutoFocusInputRef}
+              className="form-control"
+              onKeyDown={renderProps.onEnterKeyDownConfirm}
+              onChange={(e) => (title = e.target.value)}
+              placeholder={"Titulo"}
+            />
+            <br />
+            <textarea
+              type={"text"}
+              className="form-control"
+              onKeyDown={renderProps.onEnterKeyDownConfirm}
+              onChange={(e) => (description = e.target.value)}
+              placeholder={"Descripcion"}
+            />
+            <hr />
+          </form>
+        )}
+      </SweetAlert>
+    );
+  };
+
   const addNewEvent = (e, slotInfo) => {
-    var newEvents = events;
-    newEvents.push({
-      title: e,
+    const body = {
+      title: title,
+      description: description,
       start: slotInfo.start,
       end: slotInfo.end,
-    });
-    setAlert(null);
-    setEvents(newEvents);
+      userId: user.dbUser.Id,
+    };
+
+    calendarService
+      .createEvent(user.token, body)
+      .then(() => {
+        getAndSetEvents();
+      })
+      .catch((e) => console.error(e));
+
+    successAlert("El evento se guardo correctamente");
   };
 
   const hideAlert = () => {
@@ -94,8 +189,8 @@ export const Calendar = (props) => {
             localizer={localizer}
             culture={"es"}
             events={events}
-            defaultView="month"
-            scrollToTime={new Date(1970, 1, 1, 6)}
+            defaultView="week"
+            scrollToTime={new Date(1990, 1, 1, 6)}
             defaultDate={new Date()}
             onSelectEvent={(event) => selectedEvent(event)}
             onSelectSlot={(slotInfo) => addNewEventAlert(slotInfo)}
--- src/main-app/views/Calendar/Calendar.service.js
+++ src/main-app/views/Calendar/Calendar.service.js
@@ -1,13 +1,18 @@
 import axios from "axios";
 
 export const calendarService = {
-  getEvents: (auth_token) =>
-    axios.get(`${process.env.REACT_APP_API_URL}/events`, {
+  getEvents: (auth_token, user_id) =>
+    axios.get(`${process.env.REACT_APP_API_URL}/Eventos/${user_id}`, {
       headers: { Authorization: "Bearer " + auth_token },
     }),
 
   createEvent: (auth_token, body) =>
-    axios.post(`${process.env.REACT_APP_API_URL}/events`, body, {
+    axios.post(`${process.env.REACT_APP_API_URL}/Eventos`, body, {
+      headers: { Authorization: "Bearer " + auth_token },
+    }),
+
+  deleteEvent: (auth_token, event_id) =>
+    axios.delete(`${process.env.REACT_APP_API_URL}/Eventos/${event_id}`, {
       headers: { Authorization: "Bearer " + auth_token },
     }),
 };
