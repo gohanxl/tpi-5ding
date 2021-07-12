844d09c9fbfc934ebed6bb2125850835a933554b
Lucas Lacquaniti [gohanxl] <lucasmlacquaniti@gmail.com>
2021-07-12 16:56:19 -0300

    feat(Button.component.js): remove if else condition

--- src/main-app/modules/button/Button.component.js
+++ src/main-app/modules/button/Button.component.js
@@ -1,26 +1,15 @@
 import React from "react";
 import "./Button.styles.scss";
 
-const Button = ({ route, image, title }) => {
-  if (route) {
-    return (
-      <a href={`#${route}`} className="has-text-centered">
-        <div className="button-background">
-          <img src={image} alt={title} className="button-image" />
-        </div>
-        <h3 className="title is-6 button-text">{title}</h3>
-      </a>
-    );
-  } else {
-    return (
-      <a className="has-text-centered">
-        <div className="button-background">
-          <img src={image} alt={title} className="button-image" />
-        </div>
-        <h3 className="title is-6 button-text">{title}</h3>
-      </a>
-    );
-  }
+const Button = ({ route = "", image, title }) => {
+  return (
+    <a href={`#${route}`} className="has-text-centered">
+      <div className="button-background">
+        <img src={image} alt={title} className="button-image" />
+      </div>
+      <h3 className="title is-6 button-text">{title}</h3>
+    </a>
+  );
 };
 
 export default Button;
