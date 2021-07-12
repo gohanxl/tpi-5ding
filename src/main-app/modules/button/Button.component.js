daa939d5dffdae53d67b820d8fc9d8291bc5b9d9
Lucas Lacquaniti [gohanxl] <lucasmlacquaniti@gmail.com>
2021-07-12 18:07:47 -0300

    feat(Button.component.js): change anchor to button tag

--- src/main-app/modules/button/Button.component.js
+++ src/main-app/modules/button/Button.component.js
@@ -1,14 +1,19 @@
 import React from "react";
+import { useHistory } from "react-router";
 import "./Button.styles.scss";
 
 const Button = ({ route = "", image, title }) => {
+  const history = useHistory();
   return (
-    <a href={`#${route}`} className="has-text-centered">
+    <button
+      onClick={() => history.push(route)}
+      className="round-button has-text-centered"
+    >
       <div className="button-background">
         <img src={image} alt={title} className="button-image" />
       </div>
       <h3 className="title is-6 button-text">{title}</h3>
-    </a>
+    </button>
   );
 };
 
