b5b918c1640cc9767f1e68bcaaa717235e7bb5ae
Lucas Lacquaniti [gohanxl] <lucasmlacquaniti@gmail.com>
2021-07-12 16:51:34 -0300

    feat(Button.component.js): destructure props

--- src/main-app/modules/button/Button.component.js
+++ src/main-app/modules/button/Button.component.js
@@ -1,23 +1,23 @@
 import React from "react";
 import "./Button.styles.scss";
 
-const Button = (props) => {
-  if (props && props.route) {
+const Button = ({ route, image, title }) => {
+  if (route) {
     return (
-      <a href={`#${props.route}`} className="has-text-centered">
+      <a href={`#${route}`} className="has-text-centered">
         <div className="button-background">
-          <img src={props.image} alt={props.title} className="button-image" />
+          <img src={image} alt={title} className="button-image" />
         </div>
-        <h3 className="title is-6 button-text">{props.title}</h3>
+        <h3 className="title is-6 button-text">{title}</h3>
       </a>
     );
   } else {
     return (
       <a className="has-text-centered">
         <div className="button-background">
-          <img src={props.image} alt={props.title} className="button-image" />
+          <img src={image} alt={title} className="button-image" />
         </div>
-        <h3 className="title is-6 button-text">{props.title}</h3>
+        <h3 className="title is-6 button-text">{title}</h3>
       </a>
     );
   }
